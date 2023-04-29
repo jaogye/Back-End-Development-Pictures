b97447ca07f359a45147d11cff41cc6f83ff3672
jaogye <jao_ec@yahoo.com>
2023-04-29 08:58:14 -0400

    second version

--- backend/routes.py
+++ backend/routes.py
@@ -35,37 +35,77 @@ def count():
 ######################################################################
 @app.route("/picture", methods=["GET"])
 def get_pictures():
-    pass
+    if data:
+        return jsonify(data), 200
+
+    return {"message": "Internal server error"}, 500
 
 ######################################################################
 # GET A PICTURE
 ######################################################################
 
-
-@app.route("/picture/<int:id>", methods=["GET"])
+# Function to get the picture URL by ID
 def get_picture_by_id(id):
-    pass
+    for picture in data:
+        if picture['id'] == id:
+            return picture
+    return None
+
+# Flask route to get picture URL by ID
+@app.route('/picture/<int:id>', methods=['GET'])
+def get_picture(id):
+    picture_data = get_picture_by_id(id)
+    if picture_data:
+        return jsonify(picture_data)
+    else:
+        return jsonify({'error': 'Picture not found'}), 404
+
 
 
 ######################################################################
 # CREATE A PICTURE
 ######################################################################
-@app.route("/picture", methods=["POST"])
+@app.route('/picture', methods=["POST"])
 def create_picture():
-    pass
+    picture_data = request.get_json()
+    id = picture_data['id']
+    for picture in data:
+        if picture["id"] == id:
+            return jsonify({"Message": f"picture with id {id} already present"}), 302
+    
+    data.append(picture_data)    
+    return jsonify(picture_data), 201
 
 ######################################################################
 # UPDATE A PICTURE
 ######################################################################
 
 
-@app.route("/picture/<int:id>", methods=["PUT"])
+@app.route('/picture/<int:id>', methods=['PUT'])
 def update_picture(id):
-    pass
+    # Extract picture data from request body
+    picture_data = request.get_json()
+
+    picture = get_picture_by_id(id)
+    if picture:
+        picture.update(picture_data)
+        # Return a success response
+        return make_response(jsonify({"message": f"picture with id {id} updated successfully"}), 200)
+    else:        
+        return make_response(jsonify({"message": "picture not found"}), 404)
+
 
 ######################################################################
 # DELETE A PICTURE
 ######################################################################
-@app.route("/picture/<int:id>", methods=["DELETE"])
+@app.route('/picture/<int:id>', methods=['DELETE'])
 def delete_picture(id):
-    pass
+
+    picture = get_picture_by_id(id)
+    if picture:
+        data.remove(picture)
+        return make_response(jsonify({"message": f"picture with id {id} removed successfully"}), 204)
+    else:        
+        return make_response(jsonify({"message": "picture not found"}), 404)
+
+
